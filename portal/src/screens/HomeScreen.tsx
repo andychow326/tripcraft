722af91bcfec0f0a491e8fc9fbdcbe5b6de2f6ff
Andy Chow <andychchow326@gmail.com>
2024-05-02 01:52:37 +0800

    Implement initial HomeScreen

--- portal/src/screens/HomeScreen.tsx
+++ portal/src/screens/HomeScreen.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { IconGps } from "@tabler/icons-react";
+import { useTranslation } from "react-i18next";
+import cn from "classnames";
 
 const HomeScreen: React.FC = () => {
-  return <div>HomeScreen</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex flex-col divide-y-1 divide-black">
+      <div className={cn("p-4 flex flex-col gap-8 bg-yellow-100", "sm:p-8")}>
+        <h1 className={cn("text-2xl font-black", "sm:text-4xl")}>
+          {t("HomeScreen.campaign.header")}
+        </h1>
+        <p
+          className={cn(
+            "whitespace-pre-line text-lg font-semibold leading-loose",
+            "sm:text-xl",
+          )}
+        >
+          {t("HomeScreen.campaign.content")}
+        </p>
+      </div>
+      <div className={cn("p-4 flex flex-col gap-4", "sm:p-8")}>
+        <h1 className={cn("text-2xl font-black", "sm:text-4xl")}>
+          {t("HomeScreen.explore.header")}
+        </h1>
+        <div className="flex flex-row gap-2 items-center">
+          <IconGps className={cn("w-6 h-6", "sm:w-8 sm:h-8")} />
+          <span className={cn("text-xl font-semibold", "sm:text-2xl")}>
+            {t("HomeScreen.explore.popular.header")}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
