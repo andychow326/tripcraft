4aca34b10fde08c0747c29fad64adbef30d75574
Andy Chow <andychchow326@gmail.com>
2024-05-02 03:45:58 +0800

    Add change language dropdown

--- portal/src/components/ScreenLayout.tsx
+++ portal/src/components/ScreenLayout.tsx
@@ -1,4 +1,10 @@
-import React, { PropsWithChildren, useContext } from "react";
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 import {
   Button,
   Link,
@@ -6,24 +12,105 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Select,
+  SelectItem,
+  Selection,
 } from "@nextui-org/react";
 import { useTranslation } from "react-i18next";
 import { AppModalContext } from "../providers/AppModalProvider";
+import { AppLocaleContext } from "../providers/AppLocaleProvider";
+import { Locale } from "../i18n/locale";
+import { IconWorld } from "@tabler/icons-react";
+
+interface LocaleSelectorProps {
+  className?: string;
+  locale: Locale;
+  onSelectionChange: (key: Selection) => void;
+  localeOptions: {
+    label: string;
+    value: string;
+  }[];
+}
+
+const LocaleSelector: React.FC<LocaleSelectorProps> = (props) => {
+  const { className, locale, onSelectionChange, localeOptions } = props;
+
+  return (
+    <Select
+      aria-label="Locale"
+      className={className}
+      startContent={<IconWorld />}
+      labelPlacement="outside-left"
+      selectionMode="single"
+      selectedKeys={[locale]}
+      onSelectionChange={onSelectionChange}
+    >
+      {localeOptions.map((option) => (
+        <SelectItem key={option.value} value={option.value}>
+          {option.label}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+};
 
 const ScreenLayout: React.FC<PropsWithChildren> = (props) => {
   const { children } = props;
+  const [isNavMenuOpen, setIsNavMenuOpen] = useState<boolean>(false);
   const { t } = useTranslation();
   const { onOpenLoginModal, onOpenSignupModal } = useContext(AppModalContext);
+  const { locale, changeLocale } = useContext(AppLocaleContext);
+
+  const localeOptions = useMemo(
+    () =>
+      Object.keys(Locale).map((locale) => ({
+        label: t(`ScreenLayout.navbar.locale.${locale}.label`),
+        value: locale,
+      })),
+    [t],
+  );
+
+  const onSelectionChangeLocale = useCallback(
+    (key: Selection) => {
+      if (typeof key === "object") {
+        const selections = Array.from(key.values());
+        if (selections.length > 0) {
+          changeLocale(selections[0] as Locale);
+        }
+      }
+    },
+    [changeLocale],
+  );
 
   return (
     <div className="flex flex-col w-full h-full">
-      <Navbar className="border-b-1 border-black">
-        <NavbarBrand>
-          <p className="font-bold text-inherit">
-            {t("ScreenLayout.navbar.logo.name")}
-          </p>
-        </NavbarBrand>
+      <Navbar
+        className="border-b-1 border-black"
+        onMenuOpenChange={setIsNavMenuOpen}
+      >
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isNavMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <p className="font-bold text-inherit">
+              {t("ScreenLayout.navbar.logo.name")}
+            </p>
+          </NavbarBrand>
+        </NavbarContent>
         <NavbarContent justify="end">
+          <NavbarItem className="hidden sm:list-item">
+            <LocaleSelector
+              className="w-48"
+              locale={locale}
+              localeOptions={localeOptions}
+              onSelectionChange={onSelectionChangeLocale}
+            />
+          </NavbarItem>
           <NavbarItem>
             <Button
               as={Link}
@@ -45,6 +132,15 @@ const ScreenLayout: React.FC<PropsWithChildren> = (props) => {
             </Button>
           </NavbarItem>
         </NavbarContent>
+        <NavbarMenu>
+          <NavbarMenuItem>
+            <LocaleSelector
+              locale={locale}
+              localeOptions={localeOptions}
+              onSelectionChange={onSelectionChangeLocale}
+            />
+          </NavbarMenuItem>
+        </NavbarMenu>
       </Navbar>
       <div className="flex-grow">{children}</div>
     </div>
