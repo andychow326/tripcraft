6e165d722e0dfdadff85e60fd4706741341dd5ba
Emily Chan <emilychannn2002@gmail.com>
2024-05-05 03:07:14 +0800

    Add plan create button on home screen

--- portal/src/screens/HomeScreen.tsx
+++ portal/src/screens/HomeScreen.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
 import { IconGps } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
 import cn from "classnames";
+import { Button } from "@nextui-org/react";
+import { AuthContext } from "../providers/AuthProvider";
+import { AppModalContext } from "../providers/AppModalProvider";
+import { useNavigate } from "react-router-dom";
+import AppRoutes from "../AppRoutes";
 
 const HomeScreen: React.FC = () => {
   const { t } = useTranslation();
+  const { onOpenLoginModal } = useContext(AppModalContext);
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onClickStartPlanningButton = useCallback(() => {
+    if (!isAuthenticated) {
+      onOpenLoginModal();
+    } else {
+      navigate(AppRoutes.PlanCreateScreen.path);
+    }
+  }, [isAuthenticated, navigate, onOpenLoginModal]);
 
   return (
     <div className="flex flex-col divide-y-1 divide-black">
@@ -20,6 +36,13 @@ const HomeScreen: React.FC = () => {
         >
           {t("HomeScreen.campaign.content")}
         </p>
+        <Button
+          color="primary"
+          className="w-1/4 h-16 text-xl font-semibold"
+          onPress={onClickStartPlanningButton}
+        >
+          {t("HomeScreen.campaign.button.start_planning")}
+        </Button>
       </div>
       <div className={cn("p-4 flex flex-col gap-4", "sm:p-8")}>
         <h1 className={cn("text-2xl font-black", "sm:text-4xl")}>
