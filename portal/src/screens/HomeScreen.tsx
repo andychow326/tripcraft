8084747120e42568dcdd442acd180e3eade42419
Emily Chan <emilychannn2002@gmail.com>
2024-05-07 00:15:01 +0800

    Remove explore section

--- portal/src/screens/HomeScreen.tsx
+++ portal/src/screens/HomeScreen.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useContext } from "react";
-import { IconGps } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
 import cn from "classnames";
 import { Button } from "@nextui-org/react";
@@ -43,17 +42,6 @@ const HomeScreen: React.FC = () => {
         >
           {t("HomeScreen.campaign.button.start_planning")}
         </Button>
-      </div>
-      <div className={cn("p-4 flex flex-col gap-4", "sm:p-8")}>
-        <h1 className={cn("text-2xl font-black", "sm:text-4xl")}>
-          {t("HomeScreen.explore.header")}
-        </h1>
-        <div className="flex flex-row gap-2 items-center">
-          <IconGps className={cn("w-6 h-6", "sm:w-8 sm:h-8")} />
-          <span className={cn("text-xl font-semibold", "sm:text-2xl")}>
-            {t("HomeScreen.explore.popular.header")}
-          </span>
-        </div>
       </div>
     </div>
   );
